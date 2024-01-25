c02ff4c902e0a05f4930d3b625d1b5f554892c33
Enoch George <enochgeorge1999@gmail.com>
2024-01-25 15:58:51 +0300

    post confirmation lambda function and updating the schemas

---
+++ backend-flask/lambdas/cruddur_post_confirmation.py
@@ -0,0 +1,31 @@
+import json
+import os
+import psycopg2
+
+
+def lambda_handler(event, context):
+    user = event['request']['userAttributes']
+    print("User Details ----------")
+    print(user)
+    conn = None
+    sql = f"""
+        INSERT INTO public.users (display_name, email, handle, cognito_user_id)
+        VALUES('{user["name"]}', '{user["email"]}', '{user["preferred_username"]}', '{user["sub"]}');
+        """
+    print("SQL Statement Start----------")
+    print(sql)
+    print("SQL Statement End----------")
+    try:
+       conn = psycopg2.connect(os.getenv('PROD_CONNECTION_URL'))
+       cur = conn.cursor()
+       cur.execute(sql)
+       conn.commit()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            cur.close()
+            conn.close()
+            print('Database connection closed.')
+    
+    return event
